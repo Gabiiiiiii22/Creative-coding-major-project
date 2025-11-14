b6fd295adb8b5dfdd07d14341945b1b8bb0ca6a6
Sawita-Pop <sint0650@uni.sydney.edu.au>
2025-11-14 15:38:27 +1100

    update block.js

--- Classes/Block.js
+++ Classes/Block.js
@@ -11,16 +11,41 @@ class Block {
 
     display(cellSize) {
 
+        push(); // Save current transformation state
+
         // Calculate pixel position from grid coordinates
         // Multiply grid position by cell size to get pixel position
         let x = this.col * cellSize; // Left edge
         let y = this.row * cellSize; // top edge
         let w = this.colSpan * cellSize;
         let h = this.rowSpan * cellSize;
+
+        // Apply transformations to the blocks
+        translate(x + w/2, y + h/2); // Move origin to center of block
+        translate(this.offsetX, this.offsetY); // Move by animation offset
+        scale(this.scaleX, this.scaleY); // Scale from center
     
         fill(this.color); // // Set fill color for this block
         noStroke();
+
+        rectMode(CENTER);
         rect(x, y, w, h);
+
+        pop(); // Restore original transformation state
      }
 
+    update() { //Smooth animation using lerp, to be called every frame for animated blocks
+
+        this.offsetX = lerp(this.offsetX, this.targetOffsetX, 0.2);
+        this.offsetY = lerp(this.offsetY, this.targetOffsetY, 0.2);
+    }
+
+    resetTransform() { // when music stops
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.targetOffsetX = 0;
+    this.targetOffsetY = 0;
+    }
 }
